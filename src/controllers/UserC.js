022c4a8349785b27296ae2dea905c3765a531887
teustenn <teustenn@gmail.com>
2023-09-12 16:13:59 -0300

    feat: CRUD

--- src/controllers/UserC.js
+++ src/controllers/UserC.js
@@ -5,9 +5,79 @@ class UserController {
     try {
       const newUser = await User.create(req.body);
 
-      res.json(newUser);
+      return res.json(newUser);
     } catch (e) {
-      res.status(400).json({
+      return res.status(400).json({
+        errors: e.message,
+      });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+
+      return res.json(user);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      if (!req.params) {
+        return res.status(400).json({
+          errors: ['Missing id'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['User not found'],
+        });
+      }
+
+      const userUpdated = await user.update(req.body);
+      return res.json(userUpdated);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.message,
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      if (!req.params) {
+        return res.status(400).json({
+          errors: ['Missing id'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['User not found'],
+        });
+      }
+
+      const userDeleted = await user.destroy(req.body);
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({
         errors: e.message,
       });
     }
--- src/routes/userR.js
+++ src/routes/userR.js
@@ -3,6 +3,11 @@ import userController from '../controllers/UserC';
 
 const router = new Router();
 
+router.get('/', userController.index);
+router.get('/:id', userController.show);
+
 router.post('/', userController.create);
+router.put('/:id', userController.update);
+router.delete('/:id', userController.delete);
 
 export default router;
