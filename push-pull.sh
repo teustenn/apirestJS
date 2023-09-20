07722cbb968b72d334bc544c7a77940c2763cef5
Matheus Silva <teustenn@gmail.com>
2023-09-19 23:02:03 -0300

    Adjustments

--- push-pull.sh
+++ push-pull.sh
@@ -6,6 +6,6 @@ read message
 git commit -m "$message"
 git push
 ssh matheus@34.151.247.112 \
-  'cd /apirestJS ' \
+  'cd apirestJS/ ' \
   'git pull && ' \
   'pm2 restart Api && sudo service nginx restart'
