a46ca280b3cf2279719dae845a29374b5e863988
Matheus Silva <teustenn@gmail.com>
2023-09-19 23:01:12 -0300

    push.pull.sh

--- push-pull.sh
+++ push-pull.sh
@@ -1,11 +1,11 @@
 npm run build
 git add .
 
-read message
 echo "Commit Message: "
+read message
 git commit -m "$message"
 git push
-ssh api.mattheussilva.com
-  # 'git -C /home/matheus/apirestJS ' \
-  # 'git pull && ' \
-  # 'pm2 restart Api && sudo service nginx restart'
+ssh matheus@34.151.247.112 \
+  'cd /apirestJS ' \
+  'git pull && ' \
+  'pm2 restart Api && sudo service nginx restart'
